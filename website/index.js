47307f2aacd0f189a23deba52be3edc0ff1d1473
Oyame Ogbeche <oyogbeche@gmail.com>
2024-11-08 18:46:56 +0100

    updated javascript

--- website/index.js
+++ website/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter_number");
 async function updateCounter() {
     let response = await fetch("https://laii1mep7j.execute-api.eu-west-1.amazonaws.com/default/views");
     let data = await response.json()
-    counter.innerHTML = `VIEWS: ${data}`;
+    counter.innerHTML = `views: ${data}`;
 }
 
 updateCounter();
