c157d98f8a659758c4a7bfe35692d9ef956bd239
Oyame Ogbeche <oyogbeche@gmail.com>
2024-11-14 00:47:51 +0100

    updated website for mobile view

--- website/index.js
+++ website/index.js
@@ -1,8 +1,12 @@
+function toggleMenu() {
+    const navbar2 = document.querySelector(".navbar2");
+    navbar2.classList.toggle("show");
+}
+
 const counter = document.querySelector(".counter_number");
 async function updateCounter() {
     let response = await fetch("https://laii1mep7j.execute-api.eu-west-1.amazonaws.com/default/views");
     let data = await response.json()
-    counter.innerHTML = `views: ${data}`;
+    counter.innerHTML = `Views: ${data}`;
 }
-
 updateCounter();
