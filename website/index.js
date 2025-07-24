c0b26e2272309067e6981d3ac79638d3570dc5f8
Oyame Ogbeche <oyogbeche@gmail.com>
2025-07-24 12:46:03 +0100

    chore: resume update and api gateway url

--- website/index.js
+++ website/index.js
@@ -5,7 +5,7 @@ function toggleMenu() {
 
 const counter = document.querySelector(".views");
 async function updateCounter() {
-    let response = await fetch("https://laii1mep7j.execute-api.eu-west-1.amazonaws.com/default/views");
+    let response = await fetch("https://s9iv3610s5.execute-api.eu-west-1.amazonaws.com/default/views");
     let data = await response.json()
     counter.innerHTML = `This page have been viewed: ${data} times`;
 }
